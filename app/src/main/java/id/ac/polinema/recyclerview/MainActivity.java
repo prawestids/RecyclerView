9d037b8fccead5d3dce857c6a78c3ed654ea92f6
prawestids <prawestidwisalindri7@gmail.com>
2020-03-12 13:53:32 +0700

    Initial commit

---
+++ app/src/main/java/id/ac/polinema/recyclerview/MainActivity.java
@@ -0,0 +1,38 @@
+package id.ac.polinema.recyclerview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import id.ac.polinema.recyclerview.adapters.ReversedLogoAdapter;
+
+public class MainActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	public void handleSimpleButton(View view) {
+		Intent intent = new Intent(MainActivity.this,SimpleActivity.class);
+		startActivity(intent);
+	}
+
+	public void handleLogoButton(View view) {
+		Intent intent = new Intent(MainActivity.this,LogoActivity.class);
+		startActivity(intent);
+	}
+
+	public void handleGridButton(View view) {
+		Intent intent = new Intent(MainActivity.this,GridActivity.class);
+		startActivity(intent);
+	}
+
+	public void handleViewTypeButton(View view) {
+		Intent intent = new Intent(MainActivity.this, ViewTypeActivity.class);
+		startActivity(intent);
+	}
+}
